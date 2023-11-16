c6dadfcb1738fab63d05de49cde751edad146378
Anre Borges-Brown <a.borgesbrown@gmail.com>
2023-11-16 10:54:03 -0600

    new coffee

--- main.js
+++ main.js
@@ -90,6 +90,42 @@ const renderDarkCoffees = (coffees, target) => {
 	const roastDarkSelection = document.querySelector("#roast-selection");
 	renderDarkCoffees(coffees, accordionBodyDark, roastDarkSelection);
 
+	const createListElement = (toDo, toDos) => {
+		const listElement = document.createElement("li");
+		listElement.classList.add("to-do-item", "list-group-item", "d-flex", "justify-content-between", "align-items-center")
+		listElement.innerHTML = `
+        <p class="m-0">${toDo}</p>
+        <button class="btn btn-danger" data-done>Remove</button>
+    `;
+		const doneBtn = listElement.querySelector("button[data-done]");
+		doneBtn.addEventListener('click', e => {
+			listElement.remove();
+		});
+		document.querySelector("#to-do-list").appendChild(listElement);
+	};
+
+	const updateList = toDos => {
+		document.querySelector("#to-do-list").innerHTML = "";
+		for (let toDo of toDos) {
+			createListElement(toDo);
+		}
+	}
+
+
+	(() => {
+		const toDos = [];
+		const userInput = document.querySelector('#to-do');
+		const addBtn = document.querySelector('button#input');
+		updateList(toDos);
+		addBtn.addEventListener('click', e => {
+			e.preventDefault();
+			toDos.push(userInput.value);
+			updateList(toDos);
+			userInput.value = "";
+		})
+
+	})();
+
 	// submitButton.addEventListener("click", (e) => {
 	// 	updateCoffees(e, accordionBody, roastSelection);
 	// });
